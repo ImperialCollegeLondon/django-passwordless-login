addae3a4b31f9ef666da397e8aa5a727500a2a70
Adrian D'Alessandro <a.dalessandro@imperial.ac.uk>
2021-08-27 17:14:16 +0100

    No longer need ready method in appconfig

--- passwordless_login/apps.py
+++ passwordless_login/apps.py
@@ -4,6 +4,3 @@ from django.apps import AppConfig
 class PasswordlessLoginConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "passwordless_login"
-
-    def ready(self):
-        from . import settings  # noqa:F401
