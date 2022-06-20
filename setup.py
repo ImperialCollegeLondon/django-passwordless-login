d1d5bfcd125164fc7f9438c8f7ab97a4b0d63b59
Adrian D'Alessandro <a.dalessandro@imperial.ac.uk>
2022-06-20 12:49:41 +0100

    Add descriptions to setup.py

--- setup.py
+++ setup.py
@@ -5,12 +5,18 @@ from setuptools import find_packages, setup
 with Path("requirements.txt").open("r") as f:
     dependencies = f.readlines()
 
+with Path("README.md").open("r") as f:
+    long_description = f.read()
+
 setup(
     name="django-passwordless-login",
     version="0.0.1",
+    description="Login to your Django app with a link sent by email.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url="https://github.com/ImperialCollegeLondon/django-passwordless-login",
-    author="Research Computing Service, Imperial College London",
-    author_email="rcs-support@imperial.ac.uk",
+    author="Adrian D'Alessandro",
+    author_email="a.dalessandro@imperial.ac.uk",
     install_requires=dependencies,
     extras_require={},
     packages=find_packages("."),
